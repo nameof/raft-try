cac8b876e4022cae35c5841c1ee3a70063954aff
nameof <constexpr@foxmail.com>
2024-05-21 13:57:56 +0800

    node启动流程

--- src/main/java/com/nameof/raft/Node.java
+++ src/main/java/com/nameof/raft/Node.java
@@ -1,5 +1,9 @@
 package com.nameof.raft;
 
+import com.nameof.raft.config.Configuration;
+import com.nameof.raft.log.LogEntry;
+import com.nameof.raft.log.LogStorage;
+import com.nameof.raft.role.Follower;
 import com.nameof.raft.role.State;
 import lombok.Getter;
 
@@ -8,7 +12,7 @@ import java.util.Map;
 @Getter
 public class Node {
     // 持久状态
-    private int id;
+    private final int id;
     private int currentTerm;
     private Integer votedFor;
 
@@ -21,10 +25,38 @@ public class Node {
     private Map<Integer, Integer> matchIndex; // 对于每一台服务器，已知的已经复制到该服务器的最高日志条目的索引（初始值为0，单调递增）
 
     private State state;
+    private final Configuration config;
+    private final LogStorage logStorage;
+
+    public Node() {
+        config = Configuration.get();
+        id = config.getId();
+
+        commitIndex = 0;
+        lastApplied = 0;
+
+        // TODO 读取currentTerm、votedFor
+
+        logStorage = null;
+
+        setState(new Follower());
+    }
+
+    public void start() {
+        // 启动选举超时定时器
+        startElectionTimeoutTimer();
+        // 启动事件处理器
+
+        // 监听网络请求
+    }
+
+    private void startElectionTimeoutTimer() {
+        // TODO
+    }
 
     public void setState(State state) {
-        // TODO
         this.state = state;
+        // TODO state.init(this);
     }
 
     public void setCurrentTerm(int currentTerm) {
@@ -39,13 +71,19 @@ public class Node {
 
     public void setCommitIndex(int commitIndex) {
         this.commitIndex = commitIndex;
+        // TODO commit log
     }
 
     public int getLastLogTerm() {
-        return 0;
+        LogEntry last = logStorage.getLast();
+        return last == null ? -1 : last.getTerm();
     }
 
     public int getLastLogIndex() {
-        return 0;
+        return logStorage.lastIndex();
+    }
+
+    public void resetElectionTimeoutTimer() {
+        // TODO
     }
 }
