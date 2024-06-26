9cf9a47ef6db96a72f7ed951ff59f6159e86971c
nameof <constexpr@foxmail.com>
2024-06-26 11:20:16 +0800

    解决重复启动timer BUG

--- src/main/java/com/nameof/raft/handler/ElectionTimeoutHandler.java
+++ src/main/java/com/nameof/raft/handler/ElectionTimeoutHandler.java
@@ -11,9 +11,11 @@ import com.nameof.raft.rpc.Message;
 import com.nameof.raft.rpc.MessageType;
 import com.nameof.raft.rpc.Reply;
 import com.nameof.raft.rpc.Rpc;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 public class ElectionTimeoutHandler implements Handler {
 
     protected final Configuration config = Configuration.get();
@@ -27,33 +29,45 @@ public class ElectionTimeoutHandler implements Handler {
     public void handle(Node context, Message message) {
         context.setState(new Candidate());
 
+        boolean success = false;
         try {
-            requestVote(context);
+            success = requestVote(context);
         } catch (StateChangeException ignore) {
+        }
+
+        if (!success) {
+            context.resetElectionTimeoutTimer();
         }
     }
 
-    private void requestVote(Node context) {
+    private boolean requestVote(Node context) {
+        log.info("开始请求投票");
         Message.RequestVoteMessage message = buildMessage(context);
         int vote = 1;
         for (Map.Entry<Integer, NodeInfo> entry : config.getNodeMap().entrySet()) {
             Integer followerId = entry.getKey();
             Reply.RequestVoteReply reply = rpc.requestVote(config.getNodeInfo(followerId), message);
             if (reply == null) {
+                log.info("请求投票未成功：{}", followerId);
                 continue;
             }
             if (reply.isVoteGranted()) {
                 vote++;
+                log.info("获得选票：{}", followerId);
             } else {
+                log.info("选票被拒绝：{}", followerId);
                 if (reply.getTerm() > context.getCurrentTerm()) {
                     context.setState(new Follower());
                     throw new StateChangeException();
                 }
             }
         }
+        log.info("获得选票：{}个", vote);
         if (vote >= config.getMajority()) {
             context.setState(new Leader());
+            return true;
         }
+        return false;
     }
 
     private Message.RequestVoteMessage buildMessage(Node context) {
