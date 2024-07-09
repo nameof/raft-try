f327a7063012471182d2dc8d1102e655a3ad9933
nameof <constexpr@foxmail.com>
2024-07-09 10:15:44 +0800

    状态查询接口

---
+++ src/main/java/com/nameof/raft/rpc/http/StatusDto.java
@@ -0,0 +1,21 @@
+package com.nameof.raft.rpc.http;
+
+import com.nameof.raft.config.NodeInfo;
+import com.nameof.raft.log.LogEntry;
+import com.nameof.raft.role.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatusDto {
+    private NodeInfo nodeInfo;
+    private RoleType currentRole;
+    private int currentTerm;
+    private Integer voteFor;
+    private LogEntry lastLogEntry;
+}
---
+++ src/main/java/com/nameof/raft/rpc/http/StatusServlet.java
@@ -0,0 +1,32 @@
+package com.nameof.raft.rpc.http;
+
+
+import cn.hutool.http.ContentType;
+import cn.hutool.json.JSONUtil;
+import com.nameof.raft.Node;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/status", name = "StatusServlet", asyncSupported = true)
+public class StatusServlet extends HttpServlet {
+
+    private final Node context;
+
+    public StatusServlet(Node context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        StatusDto statusDto = new StatusDto(context.getConfig().getNodeInfo(), context.getState().getRole(), context.getCurrentTerm(), context.getVotedFor(), context.getLogStorage().getLast());
+
+        response.setContentType(ContentType.JSON.getValue());
+        response.getWriter().println(JSONUtil.toJsonStr(statusDto));
+        response.getWriter().flush();
+    }
+}
